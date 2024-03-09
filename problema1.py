fee8a25269534049080b5dd8e316689b2bbacaae
EjoelGt <162838921+EjoelGt@users.noreply.github.com>
2024-03-09 14:54:27 -0600

    Add files via upload

---
+++ problema1.py
@@ -0,0 +1,4 @@
+primercantidad=int(input("ingrese primer cantidad ")) 
+segundacantidad=int(input("ingrese segundad cantidad "))
+resultado=primercantidad+segundacantidad
+print("el resultado de la supa es; ", resultado)
---
+++ problema2.py
@@ -0,0 +1,3 @@
+lado=int(input("ingrese la longitud "))
+area=lado*lado
+print("el area del cuadrado es ",area) 
